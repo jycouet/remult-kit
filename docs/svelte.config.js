414e278434746d1c280ef9032a439a61df9af983
jycouet <jycouet@gmail.com>
2024-03-01 09:35:15 +0100

    :construction: WIP: docs v0

--- docs/svelte.config.js
+++ docs/svelte.config.js
@@ -1,17 +1,31 @@
-import adapter from '@sveltejs/adapter-static'
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /**
  * @type {import('@sveltejs/kit').Config}
  */
 const config = {
-  extensions: ['.svelte', '.md'],
+  extensions: [".svelte", ".md"],
   preprocess: [vitePreprocess()],
   kit: {
     adapter: adapter({
-      pages: 'dist',
+      pages: "dist",
+      fallback: "404.html",
     }),
-  },
-}
+    prerender: {
+      // handleMissingId: "ignore",
+      handleHttpError: ({ path, referrer, message }) => {
+        // TODO Why it's like this?
+        // ignore deliberate link to shiny 404 page
+        if (path === "/.https://github.com/jycouet/remult-kit") {
+          return;
+        }
 
-export default config
+        // otherwise fail the build
+        throw new Error(message);
+      },
+    },
+  },
+};
+
+export default config;
