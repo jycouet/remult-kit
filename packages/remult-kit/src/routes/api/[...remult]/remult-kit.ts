e85806377f4d989bcc119e91beda825dda0d25fb
jycouet <jycouet@gmail.com>
2024-03-13 08:14:58 +0100

    reset

--- packages/remult-kit/src/routes/api/[...remult]/remult-kit.ts
+++ packages/remult-kit/src/routes/api/[...remult]/remult-kit.ts
@@ -1,6 +1,6 @@
 import { Entity, Fields } from 'remult'
 
-import { remultKit } from '$lib/api'
+import { remultKit, type Module } from '$lib/api'
 import { auth, KitAuthUser } from '$lib/auth'
 import { github } from '$lib/auth/providers'
 
@@ -21,59 +21,63 @@ export class _AppUser extends KitAuthUser {
   jobTitle: string = 'CEO'
 }
 
+export const oAuths = [
+  github({
+    // authorizationURLOptions: { scopes: ['user:email'] },
+    // log: true,
+  }),
+]
+
 // const t: DynamicAuthorizationURLOptions<typeof oAuths> = { github: {} }
 
+export const modules: Module[] = [
+  {
+    name: 'init',
+    handlePreRemult: async (h) => {
+      h.event.setHeaders({ 'x-remult': 'hello' })
+      return await h.resolve(h.event)
+    },
+    earlyReturn: async () => {
+      return {
+        early: false,
+      }
+      // return h.resolve(h.event)
+    },
+  },
+  auth({
+    customEntities: {
+      User: _AppUser,
+    },
+
+    providers: {
+      demo: [{ name: 'Noam' }, { name: 'Ermin' }, { name: 'JYC', roles: [Role.ADMIN] }],
+
+      password: {
+        // selfSignUp: false,
+        // TODO: enable signup (bool)
+        // TODO: enable email verify (bool)
+        resetPassword: async (url) => {
+          console.info(`Mail to send with this url`, url)
+        },
+      },
+
+      otp: {
+        send: async (data) => {
+          console.info(`OTP to send`, JSON.stringify(data, null, 2))
+        },
+      },
+
+      oAuths,
+    },
+  }),
+  {
+    name: 'theEnd',
+    handlePreRemult: async (h) => {
+      return h.resolve(h.event)
+    },
+  },
+]
+
 export const remultApi = remultKit({
-  modules: [
-    {
-      name: 'init',
-      handlePreRemult: async (h) => {
-        h.event.setHeaders({ 'x-remult': 'hello' })
-        return await h.resolve(h.event)
-      },
-      earlyReturn: async () => {
-        return {
-          early: false,
-        }
-        // return h.resolve(h.event)
-      },
-    },
-    auth({
-      customEntities: {
-        User: _AppUser,
-      },
-
-      providers: {
-        demo: [{ name: 'Noam' }, { name: 'Ermin' }, { name: 'JYC', roles: [Role.ADMIN] }],
-
-        password: {
-          // selfSignUp: false,
-          // TODO: enable signup (bool)
-          // TODO: enable email verify (bool)
-          resetPassword: async (url) => {
-            console.info(`Mail to send with this url`, url)
-          },
-        },
-
-        otp: {
-          send: async (data) => {
-            console.info(`OTP to send`, JSON.stringify(data, null, 2))
-          },
-        },
-
-        oAuths: [
-          github({
-            // authorizationURLOptions: { scopes: ['user:email'] },
-            // log: true,
-          }),
-        ],
-      },
-    }),
-    {
-      name: 'theEnd',
-      handlePreRemult: async (h) => {
-        return h.resolve(h.event)
-      },
-    },
-  ],
+  modules: modules,
 })
