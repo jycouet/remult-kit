36835ec8286ee5e6505b3367cb7346a85f53ffae
jycouet <jycouet@gmail.com>
2024-04-18 14:05:07 +0200

    failng test

--- packages/remult-kit/src/lib/SqlDatabase/LogToConsoleCustom.spec.ts
+++ packages/remult-kit/src/lib/SqlDatabase/LogToConsoleCustom.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from 'vitest'
+import { expect, it } from 'vitest'
 
 import { LogToConsoleCustom } from './LogToConsoleCustom'
 
@@ -16,14 +16,24 @@ const sql = `
 				
 				limit 25 offset 0`
 
-test('First query', () => {
+it('First query', () => {
   expect(LogToConsoleCustom(0.123, sql, { $1: 'A_RENOUVELER' }, false)).toMatchInlineSnapshot(
     `"🔵 [46m123 ms [49m [36mSELECT[39m "id", ( [36mSELECT[39m SUM("enregistrementMontantHT") [35mFROM[39m [32mcontrat_periode[39m [35mWHERE[39m (contrat.id = "contratId") ), "createdAt" [35mFROM[39m [32m"contrat"[39m [35mWHERE[39m "status" = [33m'A_RENOUVELER'[39m [35mORDER[39m [35mBY[39m ( [36mSELECT[39m "dateFin" [35mFROM[39m [32mcontrat_periode[39m [35mWHERE[39m (contrat.id = "contratId") [35mORDER[39m [35mBY[39m "dateFin" DESC [35mLIMIT[39m [33m1[39m ), "id" [35mLIMIT[39m [33m25[39m [35mOFFSET[39m [33m0[39m"`,
   )
 })
 
-test('First query short', () => {
+it('First query short', () => {
   expect(LogToConsoleCustom(0.123, sql, { $1: 'A_RENOUVELER' })).toMatchInlineSnapshot(
     `"🔵 [46m123 ms [49m [36mSELECT[39m [32mcontrat[39m { status: [33m'A_RENOUVELER'[39m }[35m (sub: contrat_periode)[39m"`,
+  )
+})
+
+it('with in', () => {
+  const sql = `select 
+		"id", "updatedAt"
+		from "excel_material" where "id" in ($1,$2,$3) Order By "id"`
+
+  expect(LogToConsoleCustom(0.123, sql, { $1: 'a', $2: 'b', $3: 'c' })).toMatchInlineSnapshot(
+    `"🔵 [46m123 ms [49m [36mSELECT[39m [32mexcel_material[39m { id: [33m'a'[39m,$2,$3) }"`,
   )
 })
