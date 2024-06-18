03521c92cbbd34c54b2f26497781f81a6c7f2b07
jycouet <jycouet@gmail.com>
2024-06-18 20:39:39 +0200

    :construction: COMM - WIP: mail

---
+++ packages/remult-kit/src/lib/mail/index.ts
@@ -0,0 +1,47 @@
+import nodemailer from 'nodemailer'
+import type Mail from 'nodemailer/lib/mailer'
+
+import { Log } from '@kitql/helpers'
+
+export type MailOptions = {
+  from?: Mail.Options['from']
+  transport?: Parameters<typeof nodemailer.createTransport>[0]
+  apiUrl?: Parameters<typeof nodemailer.createTestAccount>[0]
+  info?: Parameters<typeof nodemailer.getTestMessageUrl>[0]
+}
+
+let transporter: ReturnType<typeof nodemailer.createTransport>
+let options: MailOptions | undefined
+
+export const mailInit: (o?: MailOptions) => void = (o) => {
+  options = o
+  if (o?.transport) {
+    transporter = nodemailer.createTransport(o?.transport)
+  } else {
+    // TODO Change defaults ? (it's nodemailer example!)
+    transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      secure: false, // Use `true` for port 465, `false` for all other ports
+      auth: {
+        user: 'maddison53@ethereal.email',
+        pass: 'jn7jnAPss4f63QBp6D',
+      },
+    })
+  }
+}
+
+const log = new Log('remult-kit | mail')
+
+export const sendMail: typeof transporter.sendMail = async (mailOptions) => {
+  try {
+    const info = await transporter.sendMail({
+      ...mailOptions,
+      ...{ from: mailOptions.from ?? options?.from },
+    })
+    log.success(`Sent to ${mailOptions.to}`)
+    return info
+  } catch (error) {
+    log.error(error)
+  }
+}
